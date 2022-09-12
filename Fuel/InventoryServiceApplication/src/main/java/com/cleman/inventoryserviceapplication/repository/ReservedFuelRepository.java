be79b704c39944a94f297b877f23ac371d92469f
ThanislausCleman <53737496+ThanislausCleman@users.noreply.github.com>
2022-09-12 15:29:39 +0530

    Delete Fuel directory

--- Fuel/InventoryServiceApplication/src/main/java/com/cleman/inventoryserviceapplication/repository/ReservedFuelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cleman.inventoryserviceapplication.repository;
-
-
-import com.cleman.inventoryserviceapplication.model.ReservedFuel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReservedFuelRepository extends MongoRepository<ReservedFuel, String> {
-}
